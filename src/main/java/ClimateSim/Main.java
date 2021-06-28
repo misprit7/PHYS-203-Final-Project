4d4acbceb95e2ab855ecc5dad88457efbc06168f
Xander <xandernaumenko@gmail.com>
2021-06-27 20:44:45 -0700

    Fixed weird git merge markers

--- src/main/java/ClimateSim/Main.java
+++ src/main/java/ClimateSim/Main.java
@@ -6,21 +6,9 @@ import org.knowm.xchart.XYChart;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Simulation testSim = new Simulation(30, 0.1, 200, 0.6, 0);
         testSim.run();
         testSim.graphTemp();
-//        System.out.println("Hello world");
-=======
-//        double[] xData = new double[] { 0.0, 1.0, 2.0 };
-//        double[] yData = new double[] { 2.0, 1.0, 0.0 };
-
-
-//        // Create Chart
-//        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
-//
-//        // Show it
-//        new SwingWrapper(chart).displayChart();
 
 
     }
@@ -40,6 +28,5 @@ public class Main {
 
         // Show it
         new SwingWrapper(chart).displayChart();
->>>>>>> ae3a191984db07799cb4c2b86830b83712c343a7
     }
 }
